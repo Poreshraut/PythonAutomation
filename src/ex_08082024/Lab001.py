2e8e8bd15bf0566334fb372636d841fb47b598c2
Poresh Raut <poreshraut@gmail.com>
2024-08-19 00:17:53 +0530

    Second commit

--- src/ex_08082024/Lab001.py
+++ src/ex_08082024/Lab001.py
@@ -1,3 +1,40 @@
-print("Welcome Poresh in Python Selenium Learning")
-print(2+2)
-print(2-2)
+# print("Welcome Poresh in Python Selenium Learning")
+# print(2+2)
+# print(2-2)
+# complex_number = 2 + 3j
+# print(complex_number)
+# print(complex_number.real)
+# print(complex_number.imag)
+#
+# a = 10
+# b = 10
+# c = 10
+# sum = a + b + c
+# print(sum)
+# sum = sum + 1
+# print(sum)
+# sum = sum - 1
+# print(sum)
+# sum1 = sum + 100
+# print(sum)
+# print(sum1)
+
+a = 100
+b = print(type(a))
+print(b)
+
+result = max(20, 30)
+
+a = 1
+b = 2
+
+print(f"{a+b} this is addition")
+number = 2.4578765
+formatted_number = f"{number:.3f}"
+print(formatted_number)
+print(type(formatted_number))
+
+
+print("this is the output of a + b:",a+b)
+print(f"this is the output of a + b: {a+b}")
+
